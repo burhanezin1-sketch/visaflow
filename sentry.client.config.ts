41d8d5ecb3905d38a5866fc11d0c39c3f332153c
Burhan Ezin <burhanezin@Mac.hgw.local>
2026-05-18 16:52:42 +0300

    fix: Sentry client DSN hardcode et, CSP'ye de.sentry.io ekle

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: 'https://045d552b590734e428b3406526f1217d@o4511411092324352.ingest.de.sentry.io/4511411106611280',
 
   tracesSampleRate: 0.1,
   replaysSessionSampleRate: 0,
